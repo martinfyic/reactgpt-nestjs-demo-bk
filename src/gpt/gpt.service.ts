7d93b0b569137a32fd50600e458c43779c1f9f47
martinfyic <martin.f.yic@gmail.com>
2024-09-29 20:01:52 -0300

    feat: :sparkles: creo instancia de openai en src/gpt/gpt.service.ts

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common';
 
+import OpenAI from 'openai';
+
 import { orthographyCheckCase } from './use-cases';
 import { OrthographyDto } from './dtos';
 
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckCase({ prompt: orthographyDto.prompt });
+    return await orthographyCheckCase(this.openai, {
+      prompt: orthographyDto.prompt,
+    });
   }
 }
