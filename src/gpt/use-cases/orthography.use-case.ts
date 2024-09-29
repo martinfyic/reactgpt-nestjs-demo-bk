4379f07ea54513eb43099d239e6906ffa6ee381a
martinfyic <martin.f.yic@gmail.com>
2024-09-29 20:02:42 -0300

    feat: :sparkles: implemeno openai en caso de uso orthography.use-case.ts

--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,11 +1,26 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckCase = async (options: Options) => {
+export const orthographyCheckCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
 
-  return {
-    prompt: prompt,
-  };
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o-mini-2024-07-18',
+    messages: [
+      { role: 'system', content: 'Eres un asistente muy útil y capaz.' },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+  });
+
+  console.log(completion);
+  return completion.choices[0];
 };
